d1ada2ccbc43b34b9d1306017aebe7f603a5dc76
huzikang <hzkdeyx@126.com>
2019-06-01 20:43:27 +0800

    添加视频播放按钮与停止按钮

--- source/js/daxing.js
+++ source/js/daxing.js
@@ -7,10 +7,10 @@ function daxing() {
     var leftState = 0;
     var timer = setInterval(function () {
         leftState += 0.002;
-        plan.css('left', leftState*100/3 + '%');
-        planMark.css('width', leftState*100/3 + '%');
-        leadProgress.text(Math.floor(leftState*100/3) + '%');
-        if(leftState >= 3) {
+        plan.css('left', leftState * 100 / 3 + '%');
+        planMark.css('width', leftState * 100 / 3 + '%');
+        leadProgress.text(Math.floor(leftState * 100 / 3) + '%');
+        if (leftState >= 3) {
             clearInterval(timer);
             leadProgressBody.addClass('dn');
             leadLanguage.removeClass('dn')
@@ -28,5 +28,28 @@ function daxing() {
 function playVideo() {
     $('.lead-into-page').addClass('dn');
     $('.lead-video').removeClass('dn');
-    document.getElementById('leadVideo').play();
+    var myVideo = document.getElementById('leadVideo');
+    myVideo.play();
+    setTimeout(function () {
+        myVideo.muted = true;
+    }, 12000);
+    var isMuted = true;
+    myVideo.muted = true;
+    var openBtn = $('#openSoundBtn'),
+        closeBtn = $('#closeSoundBtn');
+    openBtn.on('click', function () {
+        isMuted = false;
+        myVideo.muted = false;
+        openBtn.addClass('dn').removeClass('dlb');
+        closeBtn.removeClass('dn').addClass('dlb');
+    });
+    closeBtn.on('click', function () {
+        isMuted = true;
+        myVideo.muted = true;
+        openBtn.removeClass('dn').addClass('dlb');
+        closeBtn.addClass('dn').removeClass('dlb');
+    });
+    myVideo.addEventListener('pause', function () {
+        alert(123132)
+    })
 }
